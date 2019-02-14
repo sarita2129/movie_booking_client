cbdaf44d7f6adc0b9181105af6fb1abaf92280f5
sarita2129 <nairsarita2129@gmail.com>
2019-02-14 22:12:05 +1100

    MovieInfo changes

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-const SERVER_MOVIE_URL = 'http://localhost:3000/movies/';
+const SERVER_MOVIE_URL = 'https://moviebookingserver.herokuapp.com/movies/';
 // const SERVER_SHOW_URL = 'http://localhost:3000/shows.json';
 // const SERVER_SHOW_URL = 'http://localhost:3000/showsbycity/';
 
 class MovieInfo extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      movie: []
-    }
+ constructor(props){
+   super(props);
+   this.state = {
+     movie: []
+   }
 
-    console.log(SERVER_MOVIE_URL + this.props.match.params.movie + '.json');
-    const fetchMovie = () => {
-      // alert('1')
-        axios.get(SERVER_MOVIE_URL + this.props.match.params.movie + '.json').then((results) => {
-          this.setState({movie: results.data})
-          console.log(results.data);
-          // setTimeout(fetchSeats,4000);
-        });
+   // console.log(SERVER_MOVIE_URL + this.props.match.params.movie + '.json');
+   const fetchMovie = async () => {
+     // alert('1')
+       await axios.get(SERVER_MOVIE_URL + this.props.match.params.movie + '.json')
+       .then((results) => {
+         this.setState({movie: results.data})
+         // debugger;
+         // console.log(this.state.movie.id);
+         // console.log(this.state.movie.name);
+         // console.log(this.state.movie.image);
 
-    }
-    fetchMovie();
-    console.log("p" + this.state.movie);
-  }
-  render(){
-    return(
-      <div className="container">
+         // setTimeout(fetchSeats,4000);
+       });
+
+   }
+
+   fetchMovie();
+   //console.log("p" + this.state.movie);
+
+ //  console.log(this.state);
+ }
+
+
+ render(){
+   return(
+     <div className="container">
         <div className="row form-group">
           <div className="col-8">
-            <h1 >Movie Title</h1>
+          <h1>{this.state.movie.name}</h1>
           </div>
           <div className="col-4">
             <Link to={"/cinemas/" + this.props.match.params.city} className="btn btn-primary mT10">Book</Link>
@@ -38,23 +48,20 @@ class MovieInfo extends Component{
 
 
         </div>
+
         <div className="row form-group">
         <div className="col-8">
-        <h3>Synopsis</h3>
+            <h4>Synopsis</h4>
         </div>
         </div>
         <div className="row form-group">
         <div className="col-8">
-            <p className="ptag">loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
-            loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
-            loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
-            loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
-            loremloremloremloremloremloremloremloremlorem</p>
+            <p className="ptag">{this.state.movie.description}</p>
         </div>
         </div>
         <div className="row form-group">
           <div className="col-8">
-            <h3>Director  </h3><p>DirectorName</p>
+            <h4>Director</h4><p>{this.state.movie.director}</p>
           </div>
         </div>
         <div>
@@ -65,16 +72,17 @@ class MovieInfo extends Component{
         </div>
         <div className="row form-group">
           <div className="col-3">
-            <img src="http://www.fillmurray.com/400/700" alt="fillmurray" className="castimg"/>
-            <p>Bill Murray</p>
+            <img src={this.state.movie.castimage1} alt={this.state.movie.castname1} className="castimg"/>
+            <p>{this.state.movie.castname1}</p>
           </div>
           <div className="col-3">
-            <img src="http://www.fillmurray.com/400/700" alt="fillmurray" className="castimg"/>
-            <p>Bill Murray</p>
+            <img src={this.state.movie.castimage2} alt={this.state.movie.castname2} className="castimg"/>
+            <p>{this.state.movie.castname2}</p>
+
           </div>
           <div className="col-3">
-            <img src="http://www.fillmurray.com/400/700" alt="fillmurray" className="castimg"/>
-            <p>Bill Murray</p>
+            <img src={this.state.movie.castimage3} alt={this.state.movie.castname3} className="castimg"/>
+            <p>{this.state.movie.castname3}</p>
           </div>
         </div>
         </div>
