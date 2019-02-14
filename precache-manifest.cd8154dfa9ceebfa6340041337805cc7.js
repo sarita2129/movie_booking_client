863eeff53c90597737a0bb4795530a6e0c59b1db
sarita2129 <nairsarita2129@gmail.com>
2019-02-14 12:57:29 +1100

    Updates

--- precache-manifest.cd8154dfa9ceebfa6340041337805cc7.js
+++ precache-manifest.cd8154dfa9ceebfa6340041337805cc7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "8f675d0f7a0bf9c0c505",
+    "revision": "80ec94186762b9317b92",
     "url": "/movie_booking_client/static/css/main.4092d0fa.chunk.css"
   },
   {
-    "revision": "8f675d0f7a0bf9c0c505",
-    "url": "/movie_booking_client/static/js/main.8f675d0f.chunk.js"
+    "revision": "80ec94186762b9317b92",
+    "url": "/movie_booking_client/static/js/main.80ec9418.chunk.js"
   },
   {
     "revision": "9684e9438da7ced04a7d",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/movie_booking_client/static/js/runtime~main.2482b572.js"
   },
   {
-    "revision": "85253f0ba5147e159824f2cb5db985b1",
+    "revision": "48e8e4578ebc981df7cefbbeaee5ca6d",
     "url": "/movie_booking_client/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_booking_client/precache-manifest.8e611ecd4d103344d29df90a7e3bfe2e.js"
+  "/movie_booking_client/precache-manifest.cd8154dfa9ceebfa6340041337805cc7.js"
 );
 
 workbox.clientsClaim();
