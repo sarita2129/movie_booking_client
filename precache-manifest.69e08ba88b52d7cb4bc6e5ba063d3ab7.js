ff028dce3af0353e08d2d302eeef1748b322e626
sarita2129 <nairsarita2129@gmail.com>
2019-02-14 22:13:29 +1100

    Updates

---
+++ precache-manifest.69e08ba88b52d7cb4bc6e5ba063d3ab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25c6dd986367fd510fb7",
+    "url": "/movie_booking_client/static/css/main.683cb17c.chunk.css"
+  },
+  {
+    "revision": "25c6dd986367fd510fb7",
+    "url": "/movie_booking_client/static/js/main.25c6dd98.chunk.js"
+  },
+  {
+    "revision": "46756014b093508e046f",
+    "url": "/movie_booking_client/static/js/1.46756014.chunk.js"
+  },
+  {
+    "revision": "2482b57200d201621f87",
+    "url": "/movie_booking_client/static/js/runtime~main.2482b572.js"
+  },
+  {
+    "revision": "ab6c22443f8ab857f150433fc3761951",
+    "url": "/movie_booking_client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_booking_client/precache-manifest.1c5cedb2acc0636e8e707f63baf13857.js"
+  "/movie_booking_client/precache-manifest.69e08ba88b52d7cb4bc6e5ba063d3ab7.js"
 );
 
 workbox.clientsClaim();
